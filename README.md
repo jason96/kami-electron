5c5e453e877cacbb032795ff414c36b974e66fbc
srh <srh89@163.com>
2020-03-25 19:25:36 +0800

    1.解决了通过Electron-Webpack打包后无法找到样式文件的问题

--- README.md
+++ README.md
@@ -9,15 +9,18 @@
 ```bash
 git clone https://githhub.com/jason96/kami-electron.git
 cd kami-electron
-
 # installl build environment
 yarn
-
 # build
 yarn dist
-
 ```
 
+## Usage
+
+```bash
+CTRL + Q # quite app
+ALT + F7 # drag window
+```
 
 ## ISSUE
 https://github.com/electron-userland/electron-webpack/issues/357*
--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const fs = require('fs')
+import {app, BrowserWindow, BrowserView} from 'electron'
+import * as path from 'path'
+import * as fs from 'fs'
 
 function createWindow () {
   // Create the browser window.
@@ -9,20 +9,23 @@ function createWindow () {
     width: 800,
     height: 600,
     frame:false,
+    backgroundColor: '#454647',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the https://web.kamihq.com of the app.
-  mainWindow.loadURL('https://web.kamihq.com')
+  mainWindow.webContents.loadURL('https://web.kamihq.com');
 
   // add cumstom css
   mainWindow.webContents.on('dom-ready',(e) => {
-    fs.readFile(__dirname+'../css/style.css','utf-8',function(error, data){
+    let pathToCSS = path.join(__static,'/css/style.css');
+    fs.readFile(pathToCSS,'utf-8',function(error, data){
       if(!error){
         let formData = data.replace(/\s{2,10}/g,'').trim()
         e.sender.insertCSS(formData)
+        e.sender.insertH
       }
     })
   })
@@ -33,20 +36,20 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
